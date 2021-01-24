679d0cb9f7dfc617edfbe2d26ace9a2f941cc893
giovanni ferrara <giovanniferrara@agictech.com>
2021-01-24 20:40:47 +0100

    base line

---
+++ test/password.spec.js
@@ -0,0 +1,17 @@
+/*
+- You're not allowed to write production code unless 
+  it's to make a failing test pass
+- You're only allowed to write the one test the will 
+  make your production code fail
+- You're not allowed to write any more production code 
+  than is sufficient to pass the one failing test
+
+ questions:
+What is the unit under test (module, function, class, whatever)?
+What should it do? (Prose description)
+What was the expected output?
+
+*/
+
+import verifyPass from '../src/password'
+
