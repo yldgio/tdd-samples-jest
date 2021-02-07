5a25458c480626b1b61f8a09b093957b69872919
giovanni ferrara <giovanni.ferrara@agictech.com>
2021-02-07 21:32:06 +0100

    renamed validator

---
+++ src/verifyInput.js
@@ -0,0 +1,13 @@
+/**
+ * Verifies a value over a list of rules
+ * verifyInput
+ *  params:
+ *      val: String, the value
+ *      rules: Array, array of callbacks
+ * example:
+ *  verifyInput("giovanni.ferrara", [
+ *      (input) => input.length > 8, 
+ *      (input) => input.length < 15, 
+ * ])
+ * **/
+
