2ed5a68e34c8966cf0425ef7725dcb2d5a658dd4
giovanni ferrara <giovanni.ferrara@agictech.com>
2021-02-20 22:14:53 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import {userService} from  './services/apiServices'
+import {userService, postsService} from  './services/apiServices'
 import User from './models/user'
+
 
 
 export default async function demoAction(howManyUsers, howManyPosts){
     let result  = (await userService.list())
             .data
             .filter((el,i) => i < howManyUsers);
-    return await Promise.all(
+    const output = await Promise.all(
         result.map(async (element) => {
             let user = new User(element);
             let userPosts = await user.posts();
-            //let posts = ;
             return {
                 user: user.get(),
                 posts: userPosts.data.filter((el,i) => i < howManyPosts)
             };
     }));
+    /*
+    do posts & other stuff with output
+    */
+    return output;
 };
