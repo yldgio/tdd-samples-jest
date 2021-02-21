501b5795eeab0678f721e8b01bf52aef9e8d9fcf
giovanni ferrara <giovanni.ferrara@agictech.com>
2021-02-21 18:07:17 +0100

    SAVEPOINT

--- test/services/apiServices.spec.js
+++ test/services/apiServices.spec.js
@@ -4,18 +4,12 @@ import 'regenerator-runtime/runtime'
 import { async } from 'regenerator-runtime';
 import {emptyResponse, sampleUserResponse} from '../testHelpers'
 
-jest.mock('axios');
 
 describe("apiServices", ()=>{
     describe('userService list', ()=>{
         it('should call the right api /users', async ()=>{
-            axios.get.mockImplementationOnce(() => Promise.resolve(emptyResponse));
-            userService.list()
-            expect(axios.get).toHaveBeenCalledWith('/users');
         });
         it('should return the exact result of axios query', async ()=>{
-            axios.get.mockImplementationOnce(() => Promise.resolve(sampleUserResponse));
-            await expect(userService.list()).resolves.toEqual(sampleUserResponse);
         });
 
     });    
